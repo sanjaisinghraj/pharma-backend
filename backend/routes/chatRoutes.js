27bf52a430fbc1905dde46d1ef56df339df4c3f6
sanjaisinghraj <sanjairajsingh1@gmail.com>
2025-11-09 18:17:47 +0530

    Update chatRoutes.js

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -2,7 +2,5 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
 const { sendChat } = require("../controllers/chatController");
-
 router.post("/send", auth, sendChat);
-
 module.exports = router;
